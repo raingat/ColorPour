7c700b88ac9ee8cf7e02e8151e77e74d9a7b00fc
Vlad Midler <midlervlad@mail.ru>
2026-01-28 19:58:34 +0700

    Обновил префабы жидкостей. Улучшил логику работы жидкости и сосудов с учетом добавления Liquid Shader.

--- Assets/Source/Scripts/GameObjects/Liquid/LiquidConsumer.cs
+++ Assets/Source/Scripts/GameObjects/Liquid/LiquidConsumer.cs
@@ -1,8 +1,56 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
+[RequireComponent(typeof(Liquid))]
 public class LiquidConsumer : MonoBehaviour
 {
     [SerializeField] private VarietiesColors _color;
 
+    private Liquid _liquid;
+
+    private float _flowRate = 0.001f;
+
+    public float CurrentLevel => _liquid.planePosition.y;
+
     public VarietiesColors Color => _color;
+
+    public event Action AchievedMinLevel;
+
+    private void Awake()
+    {
+        _liquid = GetComponent<Liquid>();
+    }
+
+    public void UpLevel(float level)
+    {
+        StartCoroutine(IncreaseLevel(level));
+    }
+
+    public void DownLevel(float level)
+    {
+        StartCoroutine(DecreaseLevel(level));
+    }
+
+    private IEnumerator IncreaseLevel(float level)
+    {
+        while (_liquid.planePosition.y < level)
+        {
+            _liquid.planePosition += Vector3.up * _flowRate;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator DecreaseLevel(float level)
+    {
+        while (_liquid.planePosition.y > level)
+        {
+            _liquid.planePosition -= Vector3.up * _flowRate;
+
+            yield return null;
+        }
+
+        AchievedMinLevel?.Invoke();
+    }
 }
--- Assets/Source/Scripts/GameObjects/Preparation System/Vessel.cs
+++ Assets/Source/Scripts/GameObjects/Preparation System/Vessel.cs
@@ -5,6 +5,9 @@ public class Vessel : MonoBehaviour, IActivatable
 {
     private LiquidConsumer _captureLiquid;
 
+    private float _minLevel = -0.75f;
+    private float _maxLevel = 0.3f;
+
     public bool IsFill => _captureLiquid != null;
 
     public event Action<Vessel, LiquidConsumer> Dumping;
@@ -17,6 +20,12 @@ public class Vessel : MonoBehaviour, IActivatable
     public void Fill(LiquidConsumer liquid)
     {
         _captureLiquid = liquid;
+        _captureLiquid.transform.SetParent(transform);
+        _captureLiquid.transform.localPosition = Vector3.zero;
+
+        _captureLiquid.UpLevel(_maxLevel);
+
+        _captureLiquid.AchievedMinLevel += Clear;
     }
 
     private void TryDump()
@@ -24,7 +33,19 @@ public class Vessel : MonoBehaviour, IActivatable
         if (IsFill == false)
             return;
 
+        if (_captureLiquid.CurrentLevel < _maxLevel)
+            return;
+
+        _captureLiquid.DownLevel(_minLevel);
+    }
+
+    private void Clear()
+    {
+        _captureLiquid.AchievedMinLevel -= Clear;
+
         LiquidConsumer liquid = _captureLiquid;
+        _captureLiquid.DownLevel(_minLevel);
+        _captureLiquid.transform.SetParent(null);
         _captureLiquid = null;
 
         Dumping?.Invoke(this, liquid);
